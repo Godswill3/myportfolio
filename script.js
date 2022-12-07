611835f513f3732007dc1184a23893c1319efe40
Godswill3 <gabrielsolomon013@gmail.com>
2022-12-07 13:39:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,196 @@
+// console.log('How are you')
+// // console.log('2 + 5')
+
+// // variables
+// var person = 'Mike';
+// let item = 'Rice';
+// const num = 25;
+// person = 'John';
+// // num =30;
+
+
+// console.log(person)
+// console.log (item)
+// console.log(num)
+
+
+// // concatenation
+// console.log( 'My name is ' + person + ' i am ' + num  + 'years old')
+
+
+// // var x = 5
+// var y = 10;
+// console.log ((x + y) * 10)
+
+// // LOOP OR ITERATION
+
+// var x = 1;
+// while(x < 10){
+//     console.log('Number: ' +x)
+//     x = x + 1;
+// }
+// //  i = parseInt(prompt(1))
+// // for(let i = 1; i <= 12; i++ ){
+// //     const result = i * 2;
+
+// //     console.log(`${2} * ${i} = ${result}`)
+// // }
+// var x = 1;
+// while(x < 13){
+//     console.log('2 X ' + x + ' = '+ 2*x);
+//     x = x + 1 ;}
+
+  
+//     for (let index = 0 ; index <= 13; index++) {
+//       console.log ( '5 X ' + index +  ' = ' +5*index);
+//     }
+
+    // // condictional statement
+    // var password1 = 123456;
+    // var password2 = '123rita';
+
+    // if(password1 == 12345){
+    //   console.log('Your first password is correct');
+    // }
+
+    // else if( password2 == '123rita'){
+    //   console.log( 'Your second passord is correct');
+    // }
+
+    // else{
+    //   console.log('Unknown');d
+    // }
+
+    // some built-in functions
+    // alert('Hello Good Morning')
+    // prompt('What is your name?')
+    // confirm( 'Are you sure you are a human?')
+
+    // var info;
+    // var yob;
+    // var result;
+    // info = prompt('What is your Name?')
+    // yob = prompt( 'Enter the year you were born')
+    // result = 2022 - yob;
+
+    // alert( info  + ' you are ' + result + 'years old')
+
+    // var info;
+    // var age;
+    // info = prompt( 'Enter your Name')
+    // age = prompt('Enter your age')
+    // result = 365 * age;
+
+    // alert(info + ' you have lived for ' + result + 'days')
+
+    // var person = ['mike', 'john', 'rita', [20, 40, 50, 100]]
+    // console.log(person[3] [0])
+    // console.log(object.keys(person))
+
+    // person.forEach(element => {
+    //   console.log(persons)
+    // });
+
+    // function greet(){
+    //   console.log('Godgift is learning JavaScript')
+    // }
+
+    // greet()
+
+    // // function parameter
+    // function cal(x, y){
+    //   console.log('The result of ' +x + '/'+ y+ ' = ' + x / y)
+    // }
+
+    // cal(10, 2)
+    // cal(100, 5)
+
+  
+  //   // function return
+  //   function cal(x, y){
+  //     var result = x / y;
+  //     return result;
+  // }
+  // var answer = cal(4, 2)
+  // console.log(answer)
+  
+  // console.log(10/4)
+
+  //  function cal(x, y){
+  //     console.log('' +x + ' * ' + y + ' = ' + x * y)
+  //   }
+  //   cal( 8, 2)
+
+    // function cal (x, y ){
+    //   var result = x * y;
+    //   return result;
+    // }
+    
+    
+  //   function game(x){
+  //     var y = 1;
+  //     while(y < 13){
+  //       console.log(  x + 'X' + y + '=' + x * y);
+  //       y = y + 1;
+  //     }
+  //   }
+  // game( 4 )
+
+  // function nom(x, y){
+  //     while(y < 14){
+  //       console.log(  x + 'X' + y + '=' + x * y);
+  //       y = y + 1;
+
+  //     }
+  // }
+  //  nom(4, 9)
+
+  var info;
+  var age;
+  var value;
+  info = prompt( 'Enter your Name')
+  age = prompt('Enter your age')
+  value = prompt ('The value of multiplication table you want?')
+  function val (){
+     var y = 1;
+    while( y < 13){
+      console.log( value + 'X' + y + ' = ' + value * y)
+      y = y + 1
+    } 
+  }
+ 
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+  
